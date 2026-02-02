1ae95e13ef7827936c802cdf72fb164eb209685a
alihaiderno2 <alihaiderno2@gmail.com>
2026-02-02 21:31:05 +0500

    Build the minor classifier using mobileNet to verify the input is an x-ray photograph

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input as guard_preprocess
 import numpy as np
 from PIL import Image
 import os
@@ -9,15 +10,30 @@ import os
 def load_model():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(current_dir,'pneumonia_model.h5')
+    model2_path = os.path.join(current_dir,'guard_model.h5')
     model = tf.keras.models.load_model(model_path)
-    return model
+    guard_model = tf.keras.models.load_model(model2_path)
+    return model,guard_model
 
-model = load_model()
+model,guard_model = load_model()
 st.title("AI pneumonia Doctor")
 st.write("Upload a chest X-Ray to detect pneumonia.")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png", "jpeg"])
 
 if file is not None:
+    img_base = Image.open(file).convert('RGB')
+    guard_img = img_base.resize((224, 224))
+    guard_arr = image.img_to_array(guard_img)
+    guard_arr = np.expand_dims(guard_arr, axis=0)
+    guard_arr = guard_preprocess(guard_arr) 
+    
+    is_xray_score = guard_model.predict(guard_arr)[0][0]
+    
+    if is_xray_score < 0.10:
+        st.error("ACCESS DENIED: This image does not look like a Chest X-Ray.")
+        st.write("Please upload a valid medical scan.")
+        st.stop()
+        
     image_data = Image.open(file).convert('RGB')
     st.image(image_data,caption="Uploaded X-Ray",use_column_width=True)
     img = image_data.resize((64,64))
